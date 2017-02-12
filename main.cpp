c7c132ad7e9a955ceacf168f3fbced93cba6fad7
DrMonez <iostaleksey@gmail.com>
2017-02-12 18:47:38 +0630

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include "lib.h"
-using namespace dih;
-using namespace gold;
+using namespace optimization;
+using namespace std;
 
 void main()
 {
-	Dihotomia A;
-	A.Dih();
-	Golden_Section B;
-	B.Gold();
-	printf("Done");
-	_getch();
+	int key;
+	setlocale(LC_ALL, "Russian");
+	cout << "1 - Найти минимум функции\n2 - найти интервал, содержащий минимум функции\n";
+	cin >> key;
+	if (key == 1)
+	{
+		Dihotomia A;
+		A.Dih();
+		Golden_Section B;
+		B.Gold();
+		Fibonacci C;
+		C.Fib();
+		printf("Ваше повеление исполнено");
+		_getch();
+	}
+	else
+	{
+		if (key == 2)
+		{
+			Algoritm D;
+			D.Alg();
+		}
+		else
+		{
+			cout << "Перезапустите программу и выберете 1 или 2";
+			_getch();
+		}
+	}
 }
